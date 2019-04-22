5632050c29bb26ff8aa008eeb3a781ccf3feb09a
Yat Pan Fung <johnny@Johnnys-mac.local>
2019-04-22 13:36:24 +0800

    Updated struct .h file

---
+++ datastructure.h
@@ -0,0 +1,15 @@
+struct itemstruct{
+  idstruct productid;
+  catagorystruct catagoryid;
+  int amount;
+  string shopid;
+  string purchasedate;
+};
+struct idstruct{
+  string id;
+  string name;
+};
+struct catagorystruct{
+  string id;
+  sting name;
+};
