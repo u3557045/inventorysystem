65443e41da6c6658f2554b188ef7e5d3d5304f4b
Yat Pan Fung <johnny@Johnnys-mac.local>
2019-04-22 22:46:47 +0800

    Update datastruct,module 2

--- updatestatus.h
+++ updatestatus.h
@@ -5,21 +5,53 @@
 #include <string>
 using namespace std;
 
-void updatestatus(string filename,string productid,int amountprocured){
-  fstream inoutfile;
-  inoutfile.open(filename.c_str(),ios::app|ios::out);
-  if (inoutfile.fail()){
-    cout<<"Unable to open commodity status file\n";
-    exit(1);
+void updatestatus(string filename,int field,string content,string id){
+  fstream file,temp,amountstr;
+  string line,copy;
+  int i,count=1,amount=0;
+  bool next=true,status=false;
+  if(file.find("status")!=string::npos){
+    status=true;
   }
-
-int idpos,amountpos;
-  while (getline(inoutfile,line)){
-    //find the place of product id
-    idpos=line.find("|");
-    if (line.substr(0,idpos)==productid){
-
+  file.open(filename.c_str(),ios::in);
+  temp.open("temp_update.txt",ios::out);
+  while(getline(file,line)){
+    cout << line << endl;
+    count=1;
+    if(line.find(id)!=string::npos){
+      for(i=0;i<line.length();++i){
+        if(next){
+          temp << line[i];
+        }
+        if(line[i]=='|'){
+          count++;
+          next=true;
+        }
+        if(count==field && next){
+          temp << content << "|";
+          next=false;
+        }
+        else if(count==6)
+          amountstr+=line[i];
+        else if(count==7){
+          
+        }
+      }
+      temp << endl;
+    }
+    else{
+      temp << line <<endl;
     }
   }
+  temp.close();
+  file.close();
+  temp.open("temp_update.txt",ios::in);
+  file.open(filename,ios::out);
+  while(getline(temp,line)){
+    cout<< line <<endl;
+    file << line <<endl;
+  }
+  file.close();
+  temp.close();
 
 }
